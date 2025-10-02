29182583e0eccfebb6a1a2e107b960132c9ba032
alvin0319 <alvin7810@naver.com>
2025-10-02 14:03:02 +0900

    ...

--- install-deps.sh
+++ install-deps.sh
@@ -3,8 +3,10 @@
 git clone https://github.com/oomph-ac/dragonfly
 git clone https://github.com/oomph-ac/oomph
 
-cp patches/* oomph/
+cp patches/0001-fixes-for-latest-gophertunnel.patch oomph/
+cp patches/0001-fix-for-latest-gophertunnel-for-df.patch dragonfly/
 
 cd oomph && git am 0001-fixes-for-latest-gophertunnel.patch --whitespace=fix
+cd ../dragonfly && git am 0001-fix-for-latest-gophertunnel-for-df.patch
 
 go mod tidy && go get
